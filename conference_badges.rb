fad570beb5cf95a199da7f7b96025f284edc5144
Stephen McBride <stevemcbride3@gmail.com>
2020-04-14 20:17:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badges = []
+  names.each do |name|
+    badges.push(badge_maker(name))
+  end
+end
+
+def assign_rooms(speakers)
+  room_assignments = []
+  speakers.each_with_index do |speaker, index|
+    "Hello, #{speaker}! You'll be assigned to room #{index + 1}"
+  end
+end
